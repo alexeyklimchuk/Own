04c1437047e42a9ef380d8eb553b6f8f9037ec6e
Alexey Klimchuk <alexey.klimchuk@gmail.com>
2015-03-17 16:17:04 +0300

    added validates

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,18 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @create = {
+        title: 'Alexey Klimchuk book',
+        description: 'Book description!',
+        image_url: 'book_covers/ak47.png',
+        price: 1.01
+    }
+    @update = {
+        title: 'Alexey Klimchuk book',
+        description: 'Book description?',
+        image_url: 'book_covers/ak47.png',
+        price: 1.02
+    }
   end
 
   test "should get index" do
@@ -18,7 +30,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      # post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @create
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +48,8 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    # patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    put :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
